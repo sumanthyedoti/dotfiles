b9ca94a49f8eacb70ce1bae3c6a5c67c3b66ad99
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2022-10-20 07:07:07 +0530

    Add plugins, code-completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,14 @@
-require "user.options"
-require "user.keymaps"
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.colorscheme")
+require("user.cmp")
+require('lualine').setup {
+  options = { theme  = 'gruvbox' },
+}
 
+-- Highlight on Yank
+vim.cmd [[
+  au TextYankPost * silent! lua vim.highlight.on_yank()
+  au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=250}
+]]
