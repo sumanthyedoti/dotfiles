6ca1bb6367cca0795717de54b7a5492900271ef3
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2022-10-21 14:12:30 +0530

    Nvim: refactor autocommands

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,29 +11,10 @@ require("user.nvim-tree")
 require("user.bufferline")
 require("user.toggleterm")
 require("user.indent")
+require("user.autocommands")
 require('lualine').setup {
   options = { theme = 'gruvbox' },
 }
 
--- Highlight on Yank
-vim.cmd [[
-  au TextYankPost * silent! lua vim.highlight.on_yank()
-  au TextYankPost * silent! lua vim.highlight.on_yank {higroup="IncSearch", timeout=250}
-]]
 -- Delete without yanking if preperded by <leader>
 vim.cmd [[nnoremap <leader>dd "_dd]]
-
--- format on save
--- vim.cmd [[
---   au BufWritePre * :lua vim.lsp.buf.formatting()
--- ]]
-
--- Trim trailing whitespace
-vim.cmd [[
-  fun! TrimWhitespace()
-      let l:save = winsaveview()
-      keeppatterns %s/\s\+$//e
-      call winrestview(l:save)
-  endfun
-  au BufWritePre * :call TrimWhitespace()
-]]
