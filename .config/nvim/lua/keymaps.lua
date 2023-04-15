d784575e69b61fe4cc472f7cfe5508b1d7ed941d
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-04-15 22:52:57 +0530

    Add commands to substitute with previous yank

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -27,37 +27,45 @@ else
 end
 keymap("n", "<leader>a", "@q", opts) -- macro @q
 keymap("n", "<leader> ", ":noh<CR>", opts) -- clear search hightlight with <space><space>
+
 -- NVim-tree
 keymap("n", "<leader>e ", ":NvimTreeToggle<cr>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-TAB>", ":bprevious<CR>", opts)
 keymap("n", "<TAB>", ":bnext<CR>", opts)
+
 -- new line
 keymap("n", "<leader>o ", "o<ESC>", opts)
 keymap("n", "<leader>oo", "O<ESC>", opts)
 keymap("n", "<leader>cl", "0d$", opts)
 keymap("n", "P", "@:<CR>", opts) -- last/previous terminal command
+
 -- add semi-colon
 keymap("n", "<leader>;", "A;<esc>", opts)
 
 -- Disbale 'ZZ' command to save and quit¬
 keymap("n", "Z", ':echom "--> :w :q <-- "<CR>', opts)
 keymap("n", "ZZ", ':echom "--> :w :q <-- "<CR>', opts)
+
 -- undo all changes in the buffer
 keymap("n", "<leader>U", "<cmd>edit!<CR>", opts)
+
 -- reg menu
 keymap("n", "<leader>R", "<cmd>:reg<CR>", opts)
+
 -- splits
 keymap("n", "<leader>s ", "<cmd>vsplit<CR>", opts) -- split right
 keymap("n", "<leader>ss", "<cmd>vsplit<CR>", opts) -- split right
 keymap("n", "<leader>S", "<cmd>split<CR>", opts) -- split left
 keymap("n", "<leader>so", "<cmd>only<CR>", opts) -- kill all remaining splits
+
 -- window.split navigation
 keymap("n", "<leader>jh", "<C-w>h<CR>", opts)
 keymap("n", "<leader>jj", "<C-w>j<CR>", opts)
 keymap("n", "<leader>jk", "<C-w>k<CR>", opts)
 keymap("n", "<leader>jh", "<C-w>h<CR>", opts)
+
 -- Buffers
 keymap("n", "<leader>bo", "<cmd>BufferLineCloseLeft<CR><cmd>BufferLineCloseRight<CR>", opts) -- kill all remaining BufferLine tabs
 keymap("n", "<leader>bd", "<cmd>BufferLinePickClose<CR>", opts) -- pick close current buffer
@@ -66,16 +74,21 @@ keymap("n", "<leader>bR", "<cmd>BufferLineCloseRight<CR>", opts)
 keymap("n", "<leader>bL", "<cmd>BufferLineCloseLeft<CR>", opts)
 keymap("n", "<leader>gb", "<cmd>BufferLinePick<CR>", opts) -- pick buffer
 keymap("n", "C-b", "<cmd>bd<CR>", opts)
+
 -- treesitter playground
 keymap("n", "<leader>tp", "<cmd>TSPlaygroundToggle<CR>", opts) -- kill all remaining splits
+
 -- buffer list
 keymap("n", "<leader>bl", "<cmd>ls<cr>:b", opts)
+
 -- execute previous command
 keymap("n", "<leader>cp", ":<Up><CR>", opts)
 keymap("n", "<C-s>", ":w<CR>", opts)
+
 -- increment/descrement
 keymap("n", "-", "<C-x>", opts)
 keymap("n", "=", "<C-a>", opts)
+
 -- ## Telescope
 keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f ", "<cmd>Telescope find_files hidden=true<cr>", opts)
@@ -107,6 +120,10 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Sustitute
+keymap("n", "<leader>sp", ":%s/<C-r><C-w>/<C-r>0/g<cr>", term_opts)
+keymap("v", "<leader>sp", '"zy:%s/<C-r>z/<C-r>0/g<cr>', term_opts)
 
 -- == PLUGINS == --
 -- ## icon-picker
