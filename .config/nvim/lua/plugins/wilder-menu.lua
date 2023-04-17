b0c93d12bbfaa25a76b367f118a12f2b9b5863e4
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-04-17 08:57:13 +0530

    Fix wilder menu

--- .config/nvim/lua/plugins/wilder-menu.lua
+++ .config/nvim/lua/plugins/wilder-menu.lua
@@ -3,6 +3,32 @@ if not status_ok then
 	return
 end
 
+local gradient = {
+	"#f4468f",
+	"#fd4a85",
+	"#ff507a",
+	"#ff566f",
+	"#ff5e63",
+	"#ff6658",
+	"#ff704e",
+	"#ff7a45",
+	"#ff843d",
+	"#ff9036",
+	"#f89b31",
+	"#efa72f",
+	"#e6b32e",
+	"#dcbe30",
+	"#d2c934",
+	"#c8d43a",
+	"#bfde43",
+	"#b6e84e",
+	"#aff05b",
+}
+
+for i, fg in ipairs(gradient) do
+	gradient[i] = wilder.make_hl("WilderGradient" .. i, "Pmenu", { { a = 1 }, { a = 1 }, { foreground = fg } })
+end
+
 wilder.set_option(
 	"renderer",
 	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
@@ -13,6 +39,7 @@ wilder.set_option(
 		-- 'single', 'double', 'rounded' or 'solid'
 		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
 		border = "rounded",
+		reverse = 1,
 	}))
 )
 
