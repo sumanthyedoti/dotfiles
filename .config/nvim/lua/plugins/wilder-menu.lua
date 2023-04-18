0d5020c942bcde669bb7707301de076a2b639f5b
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-04-18 20:27:31 +0530

    Gradient for wilder-menu

--- .config/nvim/lua/plugins/wilder-menu.lua
+++ .config/nvim/lua/plugins/wilder-menu.lua
@@ -5,23 +5,12 @@ end
 
 local gradient = {
 	"#f4468f",
-	"#fd4a85",
-	"#ff507a",
-	"#ff566f",
 	"#ff5e63",
-	"#ff6658",
-	"#ff704e",
-	"#ff7a45",
 	"#ff843d",
-	"#ff9036",
 	"#f89b31",
-	"#efa72f",
 	"#e6b32e",
-	"#dcbe30",
 	"#d2c934",
-	"#c8d43a",
 	"#bfde43",
-	"#b6e84e",
 	"#aff05b",
 }
 
@@ -31,16 +20,29 @@ end
 
 wilder.set_option(
 	"renderer",
-	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-		highlighter = wilder.basic_highlighter(),
+	wilder.popupmenu_renderer({
 		highlights = {
-			border = "Normal", -- highlight to use for the border
+			gradient = gradient, -- must be set
+			-- selected_gradient key can be set to apply gradient highlighting for the selected candidate.
 		},
-		-- 'single', 'double', 'rounded' or 'solid'
-		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-		border = "rounded",
-		reverse = 1,
-	}))
+		highlighter = wilder.highlighter_with_gradient({
+			wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
+		}),
+	})
 )
+
+-- wilder.set_option(
+-- 	"renderer",
+-- 	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+-- 		highlighter = wilder.basic_highlighter(),
+-- 		highlights = {
+-- 			border = "Normal", -- highlight to use for the border
+-- 		},
+-- 		-- 'single', 'double', 'rounded' or 'solid'
+-- 		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+-- 		border = "rounded",
+-- 		reverse = 1,
+-- 	}))
+-- )
 
 wilder.setup({ modes = { ":", "/", "?" } })
