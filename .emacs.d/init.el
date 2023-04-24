238096b0ee245405d16275097b69fb498969de7c
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-04-24 23:25:07 +0530

    Emacs: install Projectile and Magit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,119 +5,145 @@
 
 (package-initialize)
 (unless package-archive-contents
-   (package-refresh-contents))
+	 (package-refresh-contents))
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+	(package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t) ; `:enusre t` for all projects
 
 ;; Download and enable Evil
 (unless (package-installed-p 'evil)
-  (package-install 'evil))
+	(package-install 'evil))
 (require 'evil)
 ;; enable evil mode
 (evil-mode 1)
 
 ;; evil-surround
 (use-package evil-surround
-  :config
-  (global-evil-surround-mode 1))
+	:config
+	(global-evil-surround-mode 1))
+
+(use-package magit
+	:commands (magit-status magit-get-current-status))
+
+(use-package rg)
 
 (use-package helm
-  ; spin-offs - helm-swoop
-  :bind
-  ("M-x" . helm-M-x)
-  ("C-x C-f" . helm-find-files)
-  ("M-y" . helm-show-kill-ring)
-  ("C-x b" . helm-mini)
-  ("M-y" . helm-show-kill-ring) ; search and insert from clipboard
-  ("C-x rb" . helm-filtered-bookmarks) ; set or search bookmark
-  ("C-s" . helm-occur)
-  :config
-  (setq helm-split-window-in-side-p t
-     helm-move-to-line-cycle-in-source t
-     helm-scroll-amount 10
-     heml-M-x-fuzzy-match t
-     helm-buffer-fuzzy-matching t
-     helm-recentf-fuzzy-match t
-     helm-semantic-fuzzy-match t
-     helm-imenu-fuzzy-match t
-     helm-apropos-fuzzy-match t)
+	; spin-offs - helm-swoop
+	:bind
+	("M-x" . helm-M-x)
+	("C-x C-f" . helm-find-files)
+	("M-y" . helm-show-kill-ring)
+	("C-x b" . helm-mini)
+	("M-y" . helm-show-kill-ring) ; search and insert from clipboard
+	("C-x rb" . helm-filtered-bookmarks) ; set or search bookmark
+	("C-s" . helm-occur)
+	:config
+	(setq helm-split-window-in-side-p t
+		 helm-move-to-line-cycle-in-source t
+		 helm-scroll-amount 10
+		 heml-M-x-fuzzy-match t
+		 helm-buffer-fuzzy-matching t
+		 helm-recentf-fuzzy-match t
+		 helm-semantic-fuzzy-match t
+		 helm-imenu-fuzzy-match t
+		 helm-apropos-fuzzy-match t)
 
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
-  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z, default is <tab>
-  (helm-mode 1)
-  (helm-autoresize-mode t)) ; automatically to fit the number of candidates
+	(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+	(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+	(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z, default is <tab>
+	(helm-mode 1)
+	(helm-autoresize-mode t)) ; automatically to fit the number of candidates
+
+(use-package projectile
+	:diminish projectile-mode
+	:bind
+	("C-c p f" . helm-projectile-find-file)
+	("C-c p p" . helm-projectile-switch-project)
+	("C-c p s" . projectile-save-project-buffers)
+	:config
+	(projectile-mode 1)
+)
+
+(use-package helm-projectile
+	:ensure t
+	:config
+	(helm-projectile-on))
 
 (use-package company
-  :ensure t
-  :diminish company-mode
-  :config
-  (add-hook 'after-init-hook #'global-company-mode)
-  ;(setq company-global-modes '(not eshell-mode comint-mode erc-mode rcirc-mode))
-  (setq company-selection-wrap-around t
-     company-tooltip-align-annotations t
-     company-idle-delay 0.3
-     company-minimum-prefix-length 2
-     company-tooltip-limit 10))
+	:ensure t
+	:diminish company-mode
+	:config
+	(add-hook 'after-init-hook #'global-company-mode)
+	;(setq company-global-modes '(not eshell-mode comint-mode erc-mode rcirc-mode))
+	(setq company-selection-wrap-around t
+		 company-tooltip-align-annotations t
+		 company-idle-delay 0.3
+		 company-minimum-prefix-length 2
+		 company-tooltip-limit 10))
 
 (use-package flycheck
-  ; supoported language - https://www.flycheck.org/en/latest/languages.html
-  :ensure t
-  :diminish flycheck-mode
-  :config
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+	; supoported language - https://www.flycheck.org/en/latest/languages.html
+	:ensure t
+	:diminish flycheck-mode
+	:config
+	(add-hook 'after-init-hook #'global-flycheck-mode))
 
 (use-package helpful ; better *help* buffer
-  :config
-  (global-set-key (kbd "C-h f") #'helpful-callable)
-  (global-set-key (kbd "C-h v") #'helpful-variable)
-  (global-set-key (kbd "C-h k") #'helpful-key)
-  (global-set-key (kbd "C-h x") #'helpful-command)
-  (global-set-key (kbd "C-c C-d") #'helpful-at-point))
+	:config
+	(global-set-key (kbd "C-h f") #'helpful-callable)
+	(global-set-key (kbd "C-h v") #'helpful-variable)
+	(global-set-key (kbd "C-h k") #'helpful-key)
+	(global-set-key (kbd "C-h x") #'helpful-command)
+	(global-set-key (kbd "C-c C-d") #'helpful-at-point))
 
 (use-package general
-  :config
-  (general-create-definer sy/leader
-   :prefix "C-c")
-  (sy/leader
-   "t" 'other-window))
+	:config
+	(general-create-definer sy/leader
+	 :prefix "C-c")
+	(sy/leader
+	 "t" 'other-window))
 
 
 (use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode)) ; prog-mode is base mode for any programming language mode
+	:hook (prog-mode . rainbow-delimiters-mode)) ; prog-mode is base mode for any programming language mode
 (use-package smartparens
-  ;:hook (prog-hook #'smartparens-mode)
-  :config
-  (smartparens-global-mode 1)
-  (show-paren-mode t))
+	;:hook (prog-hook #'smartparens-mode)
+	:config
+	(smartparens-global-mode 1)
+	(show-paren-mode t))
 
 (use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.5))
+	:init (which-key-mode)
+	:diminish which-key-mode
+	:config
+	(setq which-key-idle-delay 0.5))
 
 (use-package expand-region
-  :bind ("C-=" . er/expand-region))
+	:bind ("C-=" . er/expand-region))
+
 
 ;; TODO
-                                     ;general
-                                     ; curx
-                                     ; avy
-                                     ; yasnippet
-                                     ; ace-window
-                                     ; winner-mode
-                                     ; flycheck
+																		 ;general
+																		 ; curx
+																		 ; avy
+																		 ; yasnippet
+																		 ; ace-window
+																		 ; winner-mode
+																				; flycheck
+																				; enable flycheck
+																				; projectile
+																				; magit [https://youtu.be/INTu30BHZGk?list=PLEoMzSkcN8oPH1au7H6B7bBJ4ZO7BXjSZ&t=1769]
+
+
 
 ; ============================================
 
 ;; used by tool like git
 (setq user-full-name "Sumanth Yedoti"
-      user-mail-address "sumanth.yedoti@gmail.com")
+			user-mail-address "sumanth.yedoti@gmail.com")
 
 (setq gc-cons-threshold 10000000) ; 10mb
 (setq large-file-warning-threshold 50000000) ; 50mb
@@ -132,7 +158,7 @@
 
 ;; tab
 (setq-default tab-width 2
-              indent-tabs-mode t)
+							indent-tabs-mode t)
 
 ;; kill current buffer without asking which
 ;(global-set-key (kbd "C-x k") 'kill-this-buffer)
@@ -142,21 +168,21 @@
 
 ;; backups
 (setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
+			`((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+			`((".*" ,temporary-file-directory t)))
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; save all backup files (foo~) to this directory.
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying t    ; Don't delink hardlinks
-      version-control t      ; Use version numbers on backups
-      delete-old-versions t  ; Automatically delete excess backups
-      kept-new-versions 20   ; how many of the newest versions to keep
-      kept-old-versions 5    ; and how many of the old
-      auto-save-timeout 30   ; number of seconds idle time before auto-save
-      auto-save-interval 300) ; number of keystrokes between auto-saves
+			backup-by-copying t    ; Don't delink hardlinks
+			version-control t      ; Use version numbers on backups
+			delete-old-versions t  ; Automatically delete excess backups
+			kept-new-versions 20   ; how many of the newest versions to keep
+			kept-old-versions 5    ; and how many of the old
+			auto-save-timeout 30   ; number of seconds idle time before auto-save
+			auto-save-interval 300) ; number of keystrokes between auto-saves
 
 
 ;; ;; sort apropos results by relevancy
@@ -167,8 +193,8 @@
 (if (not (server-running-p)) (server-start))
 
 (setq inhibit-startup-screen t ; don't show the splash screen
-      ring-bell-function 'ingore ; remove bell sound, set visual bell instead
-      visible-bell t)
+			ring-bell-function 'ingore ; remove bell sound, set visual bell instead
+			visible-bell t)
 (set-face-attribute 'default nil :font "Fira Code" :height 140) ; font and font-size
 
 ;; == modes
@@ -184,14 +210,14 @@
 (global-display-line-numbers-mode -1)
 ; disable line numbers in some modes
 (dolist (mode '(org-mode-hook
-                term-mode-hook
-                shell-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+								term-mode-hook
+								shell-mode-hook
+								eshell-mode-hook))
+	(add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (setq scroll-margin 4
-      scroll-conservatively 100000
-      scroll-preserve-screen-position 1)
+			scroll-conservatively 100000
+			scroll-preserve-screen-position 1)
 
 (setq display-line-numbers 'relative)
 (tooltip-mode -1)
@@ -200,7 +226,7 @@
 
 (recentf-mode 1) ; --  M-x recentf-open-files -> to open recent files
 (setq recentf-max-saved-items 200
-      recentf-max-menu-items 20)
+			recentf-max-menu-items 20)
 
 ;; save history of mini-buffer prompts
 (savehist-mode 1)
@@ -220,54 +246,54 @@
 ;; search for more theme with `M-x list-packages` and search 'theme'
 ;; customization variable should go before load=theme call, because the theme pulls values from those variables when it gets laoded. It also means you need to re-looad the theme after you change any variable
 (setq
-      modus-themes-mode-line '(accented borderless padded)
-      modus-themes-region '(bg-only no-extend) ; text highlight
-      modus-themes-completions 'moderate ; nil | moderate | opinionated
-      modus-themes-bold-constructs t
-      modus-themes-italic-constructs t
-      modus-themes-paren-match '(bold intense) ; (bold intense underline)
-      modus-themes-prompts '(bold intense) ; (bold intense underline)
-      modus-themes-syntax '(alt-syntax green-strings yellow-comments)) ; (faint yellow-comments green-strings alt-syntax)
+			modus-themes-mode-line '(accented borderless padded)
+			modus-themes-region '(bg-only no-extend) ; text highlight
+			modus-themes-completions 'moderate ; nil | moderate | opinionated
+			modus-themes-bold-constructs t
+			modus-themes-italic-constructs t
+			modus-themes-paren-match '(bold intense) ; (bold intense underline)
+			modus-themes-prompts '(bold intense) ; (bold intense underline)
+			modus-themes-syntax '(alt-syntax green-strings yellow-comments)) ; (faint yellow-comments green-strings alt-syntax)
 
 ;; == org-mode
 ;; ! run `M-x org-mode-restart` to reflect changes org files
 (setq modus-themes-org-blocks 'tinted-background ; 'gray/tinted-background. tinted is dynamic
-      modus-themes-headings ; org-mode heading style
-      '((1 . (rainbow overline background 1.8))
-        (2 . (rainbow background 1.6))
-        (3 . (rainbow bold 1.4))
-        (t . (semilight 1.2)))
-      modus-themes-scale-headings t) ; scale heading with the above style. Important for heading style to work
+			modus-themes-headings ; org-mode heading style
+			'((1 . (rainbow overline background 1.8))
+				(2 . (rainbow background 1.6))
+				(3 . (rainbow bold 1.4))
+				(t . (semilight 1.2)))
+			modus-themes-scale-headings t) ; scale heading with the above style. Important for heading style to work
 
 ; (load-theme 'modus-vivendi t)
 (use-package all-the-icons
-  :if (display-graphic-p))
+	:if (display-graphic-p))
 (use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :config
-  (setq doom-modeline-height 36)
-  (setq doom-modeline-bar-width 10)
-  (setq doom-modeline-hud t))
+	:ensure t
+	:init (doom-modeline-mode 1)
+	:config
+	(setq doom-modeline-height 36)
+	(setq doom-modeline-bar-width 10)
+	(setq doom-modeline-hud t))
 (use-package doom-themes ; load theme with `M-x load-theme`
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t
-   doom-themes-enable-italic t
-   doom-themes-padded-modeline t)
-  (load-theme 'doom-city-lights t) ; <- change theme here
+	:ensure t
+	:config
+	;; Global settings (defaults)
+	(setq doom-themes-enable-bold t
+	 doom-themes-enable-italic t
+	 doom-themes-padded-modeline t)
+	(load-theme 'doom-gruvbox t) ; <- change theme here
 
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atoms") ; use "doom-colors" for less minimal icon theme
-  ; themes-list -> https://github.com/doomemacs/themes#theme-list
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+	;; Enable flashing mode-line on errors
+	(doom-themes-visual-bell-config)
+	;; Enable custom neotree theme (all-the-icons must be installed!)
+	(doom-themes-neotree-config)
+	;; or for treemacs users
+	(setq doom-themes-treemacs-theme "doom-atoms") ; use "doom-colors" for less minimal icon theme
+	; themes-list -> https://github.com/doomemacs/themes#theme-list
+	(doom-themes-treemacs-config)
+	;; Corrects (and improves) org-mode's native fontification.
+	(doom-themes-org-config))
 
 
 ;; ;; rebind switch-window
@@ -289,18 +315,18 @@
 ;; move customization varaibles to a seperate file and load it
 (setq customize-file (locate-user-emacs-file "~/.dotfiles/.emacs.d/customize-vars.el"))
 (load customize-file 'noerror 'nomessage)
+
+(message "Hello")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
  '(package-selected-packages
-   '(flycheck expand-region smartparens general doom-themes helpful which-key rainbow-delimiters all-the-icons doom-modeline helm use-package lua-mode evil-surround company)))
-(custom-set-faces)
+	 '(magit rg helm-projectile projectile which-key use-package spaceline smartparens smart-mode-line-powerline-theme rainbow-delimiters lua-mode helpful helm general flycheck expand-region evil-surround doom-themes doom-modeline company all-the-icons)))
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
+ )
