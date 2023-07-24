09597db4a60f8d7798c2943f30419cf503473642
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-07-24 15:36:49 +0530

    nvim ssh

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -503,6 +503,15 @@ local plugins = {
 		end,
 	},
 	{
+		"chipsenkbeil/distant.nvim",
+		branch = "v0.2",
+		config = function()
+			require("distant").setup({
+				["*"] = require("distant.settings").chip_default(),
+			})
+		end,
+	},
+	{
 		"jackMort/ChatGPT.nvim",
 		event = "VeryLazy",
 		keys = {
