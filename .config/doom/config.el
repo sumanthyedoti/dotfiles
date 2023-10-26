fc97c2ab14f11e57cf8fab18d65b9563312e239a
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2023-10-26 10:51:38 +0530

    emacs font and remove pinentry

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -26,10 +26,9 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(set-cursor-color "#ffffff") 
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 24 :weight 'semi-light)
-     doom-variable-pitch-font (font-spec :family "Hack Nerd Font Mono" :size 30)
-     doom-big-font (font-spec :family "Hack Nerd Font Mono" :size 36))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 14 :weight 'semi-light)
+     doom-variable-pitch-font (font-spec :family "Hack Nerd Font Mono" :size 20)
+     doom-big-font (font-spec :family "Hack Nerd Font Mono" :size 26))
 (after! doom-theme
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
